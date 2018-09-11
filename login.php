bfbe050623656a5789127be40d2d08bf15c05a59
Sohel Shaikh <sohelshaikh97@gmail.com>
2018-09-11 19:34:16 +0530

    Added login

--- login.php
+++ login.php
@@ -1,17 +1,31 @@
 <?php
 if (isset($_POST['submit'])) {
     include_once 'db-inc.php';
-    if ($conn){
-        echo "success";
-    }
+
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
 
-    $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT);
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = $conn->query($sql);
 
-    $sql = "INSERT INTO users(email, pass) VALUES('$email', '$hashed_pwd')";
-    $conn->query($sql);
-    header("Location: index.html?signup=sucess");
+    if ($result->num_rows > 0) {
+        if ($row = $result->fetch_assoc()){
+            $hpwd_check = password_verify($pwd, $row['pass']);
+            if ($hpwd_check == false) {
+                header("Location: fail.html?login=invalidpass");
+                exit();
+            } elseif ($hpwd_check == true) {
+                // create session here
+                // $_SESSION['email'] = $row['email'];  
+                header("Location: index.html?login=success");
+                exit();
+            }
+        }
+    }
+    else {
+        header("Location: index.html?login=invalid");
+        exit();
+    }
 
 } else {
     header("Location: index.html");
